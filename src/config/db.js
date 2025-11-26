b20354b36a349a6b2326a20a9548c2af2f023ca8
sofigonzalez14 <sofimacag@gmail.com>
2025-11-26 04:26:42 -0300

    cambios

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.DB_HOST,                       // host remoto o local
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
 });
 
 module.exports = { pool };
+
 
